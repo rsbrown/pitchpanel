46f3fbb0dcfb9d4262f6c05c2c36972b034161aa
Scott Brown <scott@rsbrown.net>
2012-10-25 08:07:18 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pitchpanel::Application.config.secret_token = 'a142165b9bc18e35675b6bf58a5a8765a9c891ae58ae3e184aaa3bb549e83f1e0b503f5e7fa33ffeadddb05294b4cdc6a696cccc81b521d85152057728bc5d09'
